9fabd28c67614145aa8b0f413f50f45a7a158ce0
Muthu308 <54983574+Muthu308@users.noreply.github.com>
2026-06-24 17:27:59 +0530

    Update adminuserman.js

--- api/adminuserman.js
+++ api/adminuserman.js
@@ -1,50 +1,39 @@
+const HASURA_URL = process.env.HASURA_URL;
+const HASURA_SECRET = process.env.HASURA_SECRET;
+
 async function hasura(query, variables = {}) {
+  const response = await fetch(HASURA_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-hasura-admin-secret": HASURA_SECRET
+    },
+    body: JSON.stringify({
+      query,
+      variables
+    })
+  });
 
-  const res = await fetch(
-    HASURA_URL,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-hasura-admin-secret":
-          HASURA_SECRET
-      },
-      body: JSON.stringify({
-        query,
-        variables
-      })
-    }
-  );
+  const json = await response.json();
 
-  if (!res.ok) {
+  if (!response.ok) {
     throw new Error(
-      `HTTP ${res.status}`
+      json?.errors?.[0]?.message ||
+      `HTTP ${response.status}`
     );
   }
 
-  const json =
-    await res.json();
-
   if (json.errors) {
-    throw new Error(
-      json.errors[0].message
-    );
+    throw new Error(json.errors[0].message);
   }
 
   return json.data;
 }
 
 const GQL = {
-
   LIST_USERS: `
     query {
-
-      user_student(
-        order_by:{
-          user_id:desc
-        }
-      ) {
-
+      user_student(order_by:{user_id:desc}) {
         user_id
         name
         email_id
@@ -57,21 +46,13 @@ const GQL = {
         active_status
         user_role
         created_at
-
       }
-
     }
   `,
 
   GET_USER: `
-    query GetUser(
-      $user_id:Int!
-    ){
-
-      user_student_by_pk(
-        user_id:$user_id
-      ){
-
+    query GetUser($user_id:Int!) {
+      user_student_by_pk(user_id:$user_id) {
         user_id
         name
         email_id
@@ -83,23 +64,15 @@ const GQL = {
         dob
         active_status
         user_role
-
       }
-
     }
   `,
 
   CREATE_USER: `
-    mutation CreateUser(
-      $object:user_student_insert_input!
-    ){
-
-      insert_user_student_one(
-        object:$object
-      ){
+    mutation CreateUser($object:user_student_insert_input!) {
+      insert_user_student_one(object:$object) {
         user_id
       }
-
     }
   `,
 
@@ -107,101 +80,87 @@ const GQL = {
     mutation UpdateUser(
       $user_id:Int!,
       $set:user_student_set_input!
-    ){
-
+    ) {
       update_user_student_by_pk(
-        pk_columns:{
-          user_id:$user_id
-        }
+        pk_columns:{user_id:$user_id}
         _set:$set
-      ){
+      ) {
         user_id
       }
-
     }
   `,
 
   DELETE_USER: `
-    mutation DeleteUser(
-      $user_id:Int!
-    ){
-
-      delete_user_student_by_pk(
-        user_id:$user_id
-      ){
+    mutation DeleteUser($user_id:Int!) {
+      delete_user_student_by_pk(user_id:$user_id) {
         user_id
       }
-
     }
   `
 };
 
-const API = {
+export default async function handler(req, res) {
+  try {
+    const { action, payload } = req.body || {};
 
-  async getUsers() {
+    let result;
 
-    const data =
-      await hasura(
-        GQL.LIST_USERS
-      );
+    switch (action) {
+      case "list":
+        result = await hasura(GQL.LIST_USERS);
+        return res.status(200).json(result.user_student);
 
-    return data.user_student;
-  },
+      case "get":
+        result = await hasura(
+          GQL.GET_USER,
+          { user_id: payload.user_id }
+        );
+        return res.status(200).json(
+          result.user_student_by_pk
+        );
 
-  async getUser(user_id) {
+      case "create":
+        result = await hasura(
+          GQL.CREATE_USER,
+          { object: payload }
+        );
+        return res.status(200).json(
+          result.insert_user_student_one
+        );
 
-    const data =
-      await hasura(
-        GQL.GET_USER,
-        { user_id }
-      );
+      case "update":
+        result = await hasura(
+          GQL.UPDATE_USER,
+          {
+            user_id: payload.user_id,
+            set: payload.set
+          }
+        );
+        return res.status(200).json(
+          result.update_user_student_by_pk
+        );
 
-    return data.user_student_by_pk;
-  },
+      case "delete":
+        result = await hasura(
+          GQL.DELETE_USER,
+          {
+            user_id: payload.user_id
+          }
+        );
+        return res.status(200).json(
+          result.delete_user_student_by_pk
+        );
 
-  async createUser(fields) {
+      default:
+        return res.status(400).json({
+          error: "Invalid action"
+        });
+    }
+  } catch (err) {
+    console.error(err);
 
-    const data =
-      await hasura(
-        GQL.CREATE_USER,
-        {
-          object: fields
-        }
-      );
-
-    return data.insert_user_student_one;
-  },
-
-  async updateUser(
-    user_id,
-    set
-  ) {
-
-    const data =
-      await hasura(
-        GQL.UPDATE_USER,
-        {
-          user_id,
-          set
-        }
-      );
-
-    return data.update_user_student_by_pk;
-  },
-
-  async deleteUser(
-    user_id
-  ) {
-
-    const data =
-      await hasura(
-        GQL.DELETE_USER,
-        {
-          user_id
-        }
-      );
-
-    return data.delete_user_student_by_pk;
+    return res.status(500).json({
+      error: err.message
+    });
   }
-
-};
+}
