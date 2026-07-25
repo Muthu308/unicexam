24675031330e7a557cafe9cc3ddb36b5cb4673a1
Muthu308 <54983574+Muthu308@users.noreply.github.com>
2026-07-25 22:19:59 +0530

    Update classfeedback.js

--- api/classfeedback.js
+++ api/classfeedback.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
               rating
               feedback_text
               created_at
-              classes {
+              class {
                 id
                 subject
                 class_date
